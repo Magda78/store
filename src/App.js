3707f3897a5eec857db97be63edc2d4dd4f1257c
Magda <magda.78@hotmail.com>
2020-01-20 13:49:27 -0500

    Implemented firebase

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {Switch, Route} from 'react-router-dom';
 import ShopPage from './page/shop/shop';
 import Header from './components/header/header';
 import SignInAndSignUpPage from './page/sign-in-and-sign-up/sign-in-and-sign-up';
-import {auth} from './/firebase/firebase';
+import {auth, createUserProfileDocument} from './/firebase/firebase';
 
 
 
@@ -17,9 +17,20 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser:user});
-    
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+            });
+            console.log(this.state);
+          })
+      }
+      this.setState({currentUser: userAuth});
     })
   }
 
